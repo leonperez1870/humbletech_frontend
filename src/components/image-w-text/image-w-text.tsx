84e3c32de97d8132092185f346ef616a936b4d6b
leonperez1870 <leoncioperez512@gmail.com>
2023-05-23 17:22:05 -0400

    Added conditionals for each element

---
+++ src/components/image-w-text/image-w-text.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { GatsbyImage, IGatsbyImageData, getImage } from 'gatsby-plugin-image';
+
+interface ImgWithTextProps {
+  sectionImage: IGatsbyImageData;
+  sectionHeading: string;
+  sectionSubHeading: string;
+  imageOnRight?: boolean;
+}
+
+const ImgWithText: React.FC<ImgWithTextProps> = ({
+  sectionImage,
+  sectionHeading,
+  sectionSubHeading,
+  imageOnRight
+}) => {
+  const secImg = getImage(sectionImage);
+  
+  return (
+    <section aria-label="Information section">
+      {sectionImage && (
+        <div className={`flex items-center justify-center ${imageOnRight ? 'flex-row-reverse' : ''}`}>
+          <GatsbyImage image={secImg} alt="Section Image" className="w-1/2"/>
+          <div className="text-center text-black w-1/2">
+            { sectionHeading && <h1 className="text-3xl mb-4">{sectionHeading}</h1>}
+            { sectionSubHeading && <h2 className="text-xl">{sectionSubHeading}</h2>}
+          </div>
+        </div>
+      )}
+    </section>
+  )
+}
+
+export default ImgWithText;
