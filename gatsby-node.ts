249db7a4dbcc2872fc9609660abfe2eab8e5e219
leonperez1870 <leoncioperez512@gmail.com>
2023-05-23 17:20:49 -0400

    Dynamically creates pages according to contentful

---
+++ gatsby-node.ts
@@ -0,0 +1,96 @@
+// gatsby-node.ts
+
+import { GatsbyNode } from 'gatsby';
+import path from 'path';
+
+export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  // Query all Contentful page data
+  const result = await graphql<{
+    allContentfulPages: {
+      edges: {
+        node: {
+          id: string;
+          title: string;
+          slug: string;
+          sections: {
+            __typename: string;
+            id: string;
+            // Include other fields from the Section types as necessary
+            // For a "Hero" type
+            backgroundImageDesktop?: {
+              gatsbyImageData: any;
+            };
+            backgroundImageMobile?: {
+              gatsbyImageData: any;
+            };
+            heading?: string;
+            subHeading?: string;
+            // For a "ImageWithText" type
+            imageOnRight?: boolean;
+            sectionHeading?: string;
+            sectionImage?: {
+              gatsbyImageData: any;
+            };
+            sectionSubHeading?: string;
+            spaceId?: string;
+          }[];
+        };
+      }[];
+    };
+  }>(`
+    query ContentfulPagesQuery {
+      allContentfulPages {
+        edges {
+          node {
+            id
+            title
+            slug
+            sections {
+              __typename
+              ... on ContentfulHero {
+                id
+                backgroundImageDesktop {
+                  gatsbyImageData
+                }
+                backgroundImageMobile {
+                  gatsbyImageData
+                }
+                heading
+                subHeading
+              }
+              ... on ContentfulImageWithText {
+                id
+                imageOnRight
+                sectionHeading
+                sectionImage {
+                  gatsbyImageData
+                }
+                sectionSubHeading
+                spaceId
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    throw result.errors;
+  }
+
+  // Create pages for each Contentful page
+  result.data?.allContentfulPages.edges.forEach(({ node }) => {
+    createPage({
+      path: `/${node.slug}`,
+      component: path.resolve(`./src/templates/page.tsx`),
+      context: {
+        id: node.id,
+        title: node.title,
+        sections: node.sections,
+      },
+    });
+  });
+};
