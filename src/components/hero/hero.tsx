8a4efe85b01a83f9a7f43b064c96e712573e5418
leonperez1870 <leoncioperez512@gmail.com>
2023-05-23 17:21:33 -0400

    Added mobile image and conditional logic to show/hide elements

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,29 +1,46 @@
-import * as React from 'react';
-import { GatsbyImage } from 'gatsby-plugin-image';
+import React, { useState, useEffect } from 'react';
+import { GatsbyImage, IGatsbyImageData, getImage } from 'gatsby-plugin-image';
 
 interface HeroProps {
   backgroundImageDesktop: IGatsbyImageData;
-  heroHeading: string;
-  heroSubHeading: string;
+  backgroundImageMobile: IGatsbyImageData;
+  heading: string;
+  subHeading: string;
   ctaText: string;
   ctaUrl: string;
 }
 
 const Hero: React.FC<HeroProps> = ({
   backgroundImageDesktop,
-  heroHeading,
-  heroSubHeading,
+  backgroundImageMobile,
+  heading,
+  subHeading,
   ctaText,
   ctaUrl
-}) => (
-  <div className="Hero h-screen flex items-center justify-center bg-cover bg-center relative">
-    <GatsbyImage image={backgroundImageDesktop} alt="Background image for the hero section" className="absolute inset-0 w-full h-full z-0" style={{filter: 'brightness(0.8)'}} />
-    <div className="Hero-content text-center text-white relative">
-      <h1 className="text-5xl mb-4">{heroHeading}</h1>
-      <h2 className="text-2xl mb-8">{heroSubHeading}</h2>
-      <a href={ctaUrl} className="bg-blue-500 text-white px-6 py-3 rounded-lg">{ctaText}</a>
+}) => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const desktopBgImg = getImage(backgroundImageDesktop),
+        mobileBgImg = getImage(backgroundImageMobile);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 640);
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const backgroundImage = isMobile ? mobileBgImg : desktopBgImg;
+
+  return (
+    <div className="Hero h-screen flex items-center justify-center bg-cover bg-center relative">
+      { backgroundImage && <GatsbyImage image={backgroundImage} alt="Background image for the hero section" className="absolute inset-0 w-full h-full z-0" style={{filter: 'brightness(0.8)'}} /> }
+      { heading && <div className="Hero-content text-center text-white relative">
+        <h1 className="text-5xl mb-4">{heading}</h1>
+        { subHeading && <h2 className="text-2xl mb-8">{subHeading}</h2> }
+        { ctaText && ctaUrl && <a href={ctaUrl} className="bg-blue-500 text-white px-6 py-3 rounded-lg">{ctaText}</a> }
+      </div> }
     </div>
-  </div>
-)
+  )
+} 
 
 export default Hero;
