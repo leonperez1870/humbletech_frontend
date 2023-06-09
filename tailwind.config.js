4a7082a5f683b774ea7ecc8d6e79200e7a51e12e
leonperez1870 <leoncioperez512@gmail.com>
2023-06-08 22:46:52 -0400

    included templates for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}"
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/templates/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
