8df5a1fe02dea542896cfcfe6544c89b2b639eea
leonperez1870 <leoncioperez512@gmail.com>
2023-06-10 23:52:59 -0400

    applied cart drawer to layout

--- src/templates/layout.tsx
+++ src/templates/layout.tsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
+import ShoppingCart from "../components/shopping-cart/shopping-cart";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <div>
-    <Header siteTitle="The Humble Tech" />
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
-    <main>{children}</main>
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
 
-    <Footer />
-  </div>
-);
+  return (
+    <div>
+      <Header siteTitle="The Humble Tech" toggleCart={toggleCart} />
+      <main>{children}</main>
+      {isCartOpen && <ShoppingCart />}
+      <Footer />
+    </div>
+  );
+}
 
 export default Layout;
