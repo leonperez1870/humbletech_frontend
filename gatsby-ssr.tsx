92be46d1eb646e5555ff1a62d95a46ff38f90e8a
leonperez1870 <leoncioperez512@gmail.com>
2023-06-10 23:54:36 -0400

    wrapped shopify

---
+++ gatsby-ssr.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ShopifyProvider } from './src/utils/shopify/shop';
+
+export const wrapRootElement = ({ element }: { element: React.ReactNode }) => (
+  <ShopifyProvider>{element}</ShopifyProvider>
+);
