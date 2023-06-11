88fec1c5331a598d1731d833c5397cde327677e7
leonperez1870 <leoncioperez512@gmail.com>
2023-06-10 23:55:50 -0400

    applied cart button and set toggle to open/close the drawer

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,9 +3,10 @@ import { Link } from 'gatsby';
 
 interface HeaderProps {
   siteTitle?: string;
+  toggleCart: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ siteTitle = '' }) => {
+const Header: React.FC<HeaderProps> = ({ siteTitle = '', toggleCart = () => {} }) => {
   return (
     <header className="p-5 bg-white shadow-md fixed top-0 w-full z-50">
       <div className="container mx-auto flex justify-between items-center">
@@ -17,8 +18,14 @@ const Header: React.FC<HeaderProps> = ({ siteTitle = '' }) => {
           <Link to="/contact" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="Contact">Contact</Link>
           <Link to="/portfolio" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="Portfolio">Portfolio</Link>
           <Link to="/services" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="Services">Services</Link>
+          <Link to="/collections/merch" className="text-gray-700 hover:text-indigo-600 transition duration-300">Merch</Link>
           <Link to="/register" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="Register">Register</Link>
           <Link to="/login" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="Login">Login</Link>
+          <button onClick={() => {
+            toggleCart();
+          }} className="text-gray-700 hover:text-indigo-600 transition duration-300">
+            Cart
+          </button>
         </nav>
       </div>
     </header>
