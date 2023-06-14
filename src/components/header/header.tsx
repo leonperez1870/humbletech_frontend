d4683186b915b5c1d5448203b3cfefe7ea3b4bf6
leonperez1870 <leoncioperez512@gmail.com>
2023-06-14 12:43:52 -0400

    removal of some buttons

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -16,11 +16,8 @@ const Header: React.FC<HeaderProps> = ({ siteTitle = '', toggleCart = () => {} }
         <nav className="space-x-4" aria-label="Main">
           <Link to="/about" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="About">About</Link>
           <Link to="/contact" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="Contact">Contact</Link>
-          <Link to="/portfolio" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="Portfolio">Portfolio</Link>
           <Link to="/services" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="Services">Services</Link>
           <Link to="/collections/merch" className="text-gray-700 hover:text-indigo-600 transition duration-300">Merch</Link>
-          <Link to="/register" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="Register">Register</Link>
-          <Link to="/login" className="text-gray-700 hover:text-indigo-600 transition duration-300" aria-label="Login">Login</Link>
           <button onClick={() => {
             toggleCart();
           }} className="text-gray-700 hover:text-indigo-600 transition duration-300">
