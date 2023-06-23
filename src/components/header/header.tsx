ff1df9d5e53757b6fe141489766b96155f9198ce
leonperez1870 <leoncioperez512@gmail.com>
2023-06-23 15:13:57 -0400

    header styling for desktop and mobile

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -15,17 +15,22 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ siteTitle = '', toggleCart = () => {}, navigation = [], headerLogo }) => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const logoImage = getImage(headerLogo);
+
   return (
-    <header className="Header p-5 shadow-md fixed top-0 w-full z-50">
+    <header className="Header p-5 shadow-md fixed top-0 w-full z-50 bg-white">
       <div className="container mx-auto flex justify-between items-center">
-        <div>
+        <div className="flex items-center justify-between w-1/2 mobile-container">
           <Link to="/" className="text-xl font-bold text-gray-800 Header__logo-title" aria-label="Home">
             {logoImage && <GatsbyImage image={logoImage} alt={siteTitle} className="w-24 rounded-full" />}
-            <p>{siteTitle}</p>
+            <p className='uppercase'>{siteTitle}</p>
           </Link>
+          <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="Header__mobile-menu">
+            <i className="icon icon-menu" aria-label="menu" />
+          </button>
         </div>
-        <nav className="space-x-4" aria-label="Main">
+        <nav className={`Header__navigation ${isMenuOpen ? 'menu-open' : 'menu-close'} flex items-center space-x-4`} aria-label="Main">
           {navigation.map((navItem, index) => {
             return (
               <Link key={index} to={navItem.navUrl} className="text-gray-700 hover:text-indigo-600 transition duration-300">
@@ -34,7 +39,7 @@ const Header: React.FC<HeaderProps> = ({ siteTitle = '', toggleCart = () => {}, 
             );
           })}
           <button onClick={toggleCart} className="text-gray-700 hover:text-indigo-600 transition duration-300">
-            Cart
+            <i className="icon icon-shopping-bag" aria-label="shopping bag" />
           </button>
         </nav>
       </div>
