a8f6fd7a23f14c579f6c45480492f72a101570fe
leonperez1870 <leoncioperez512@gmail.com>
2023-06-23 15:15:18 -0400

    tailwind styling

--- src/components/image-w-text/image-w-text.tsx
+++ src/components/image-w-text/image-w-text.tsx
@@ -18,15 +18,17 @@ const ImgWithText: React.FC<ImgWithTextProps> = ({
   
   return (
     <section aria-label="Information section">
-      {sectionImage && (
-        <div className={`flex items-center justify-center ${imageOnRight ? 'flex-row-reverse' : ''}`}>
-          <GatsbyImage image={secImg} alt="Section Image" className="w-1/2"/>
-          <div className="text-center text-black w-1/2">
-            { sectionHeading && <h1 className="text-3xl mb-4">{sectionHeading}</h1>}
-            { sectionSubHeading && <h2 className="text-xl">{sectionSubHeading}</h2>}
+      <div className='container--xl'>
+        {sectionImage && (
+          <div className={`flex items-center justify-center ${imageOnRight ? 'flex-row-reverse' : ''}`}>
+            <GatsbyImage image={secImg} alt="Section Image" className="w-1/2"/>
+            <div className="text-center text-black w-1/2">
+              { sectionHeading && <h1 className="text-3xl mb-4">{sectionHeading}</h1>}
+              { sectionSubHeading && <h2 className="text-xl">{sectionSubHeading}</h2>}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </section>
   )
 }
